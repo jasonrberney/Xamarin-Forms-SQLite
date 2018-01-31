a927131008cdb809ed371133859b34bda68f163b
Jason Berney <jasonrberney@gmail.com>
2018-01-31 10:36:35 -0800

    initial commit for Xamarin-Forms-SQLite.

---
+++ MyBooks/MyBooks/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace MyBooks
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void ToolbarItem_Activated(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NewBookPage());
+        }
+    }
+}
---
+++ MyBooks/MyBooks/NewBookPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyBooks
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewBookPage : ContentPage
+	{
+		public NewBookPage ()
+		{
+			InitializeComponent ();
+		}
+
+        
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Success", "We have handled the click event", "Thank you");
+        }
+    }
+}
