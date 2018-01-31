61fbf3af7ba511b8bcf7559ddabcb8f4c66d1f9a
Jason Berney <jasonrberney@gmail.com>
2018-01-31 11:01:52 -0800

    Add Book class. Add DB_Path to App.

--- MyBooks/MyBooks/App.xaml.cs
+++ MyBooks/MyBooks/App.xaml.cs
@@ -9,12 +9,22 @@ namespace MyBooks
 {
 	public partial class App : Application
 	{
+        public static string DB_PATH = string.Empty;
 		public App ()
 		{
 			InitializeComponent();
 
             MainPage = new NavigationPage(new MainPage());
 		}
+
+        public App(string DB_PATH)
+        {
+            InitializeComponent();
+
+            DB_PATH = DB_PATH;
+
+            MainPage = new NavigationPage(new MainPage());
+        }
 
 		protected override void OnStart ()
 		{
